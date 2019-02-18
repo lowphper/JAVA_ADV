43c1875021607254366bf77460565292eb22e3d6
陈佳飞 <1175843850@qq.com>
2019-02-18 18:58:05 +0800

    juc相关知识1.0:  线程的六种状态

---
+++ juc/src/main/java/com/fei/juc/base/Demo2.java
@@ -0,0 +1,8 @@
+package com.fei.juc.base;
+
+public class Demo2 {
+    /**
+     * 并行与并发的区别
+     *
+     */
+}
---
+++ juc/src/main/java/com/fei/juc/base/ThreadStatus.java
@@ -0,0 +1,16 @@
+package com.fei.juc.base;
+
+public class ThreadStatus {
+    public static void main(String[] args) {
+        /**
+         * 线程的六种状态 在线程类Thread类中有个 public enum State，六种状态
+         * 1 NEW 创建
+         * 2 RUNNABLE 运行状态
+         * 3 LOCKED 锁状态
+         * 4 WAITING 等待状态
+         * 5 TIMED_WAITING 设定时间的等待状态
+         * 6 TERMINATED
+         */
+        new Thread();
+    }
+}
