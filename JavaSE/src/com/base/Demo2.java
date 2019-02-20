dcb17a8f1d9cc60530c13b4f0617a68dc952221e
陈佳飞 <1175843850@qq.com>
2019-02-20 17:19:43 +0800

    2.1  1.增加JavaSE基础知识  2.增加MySQL知识

---
+++ JavaSE/src/com/base/Demo2.java
@@ -0,0 +1,13 @@
+package com.base;
+
+import java.util.HashMap;
+
+/**
+ * HashMap源码分析
+ *
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        new HashMap<>();
+    }
+}
---
+++ MySQL/src/base/Demo2.java
@@ -0,0 +1,24 @@
+package base;
+
+import com.sun.org.apache.bcel.internal.generic.Select;
+
+import javax.print.attribute.standard.JobMessageFromOperator;
+
+/**
+ * 1为什么要使用索引：
+ *   因为对于大数据表，查询的数据可能是上百万行，如果全表扫描查询，速度很慢
+ *   通过使用索引，可以快速查询
+ *   就像查字典
+ * 2二叉树查找原理：
+ *   二叉树查找，头部有一个数字A，A左边的数字都比它小，右边的数字都比它大，基本如图所示，查询效率为O(logn)
+ *
+ *                  78
+ *          45              123
+ *      34    47        111     135
+ *    21  39 40 56    102  115 132  167
+ *
+ */
+public class Demo2 {
+    Select * JobMessageFromOperator
+
+}
